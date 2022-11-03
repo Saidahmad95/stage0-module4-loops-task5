a888d27c578eb838958825e4d73664defc38e04e
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-03 19:50:58 +0500

    first commit

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int i = cathetusLength; i >= 1; i--) {
+            for (int j = i; j <= cathetusLength; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
